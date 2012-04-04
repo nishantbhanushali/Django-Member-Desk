6390feccd05655f73e0d893bfefba93c7e8d4b0a
Leon Klepfish <klepfish@gmail.com>
2012-04-04 15:36:20 -0700

    members forms

--- memberdesk/apps/members/urls.py
+++ memberdesk/apps/members/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
-
+from django.views.generic import DetailView, ListView, UpdateView, CreateView
+from members.models import Member
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "members/member_list.html"}, name="member_list"),
+    url(r'^$', ListView.as_view(model=Member,template_name='members/index.html')),
+    url(r'^create/$', CreateView.as_view(model=Member)),
+    url(r'^(?P<pk>\d+)/$', UpdateView.as_view(model=Member)),
+
 )
