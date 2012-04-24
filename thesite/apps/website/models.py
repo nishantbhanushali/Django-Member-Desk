05b3829e5a987f48ac00fcb920a4b645fe900b0e
Leon Klepfish <klepfish@gmail.com>
2012-04-24 09:41:00 -0700

    fixing up website models

--- thesite/apps/website/models.py
+++ thesite/apps/website/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Website(models.Model):
     name = models.CharField(max_length=128)
-    default_title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
     support_email = models.CharField(max_length=128)
     support_link = models.CharField(max_length=128)
     jv_password = models.CharField(max_length=128)
+    cb_username = models.CharField(max_length=128)
+    cb_secret = models.CharField(max_length=128)
+    paypal_email = models.CharField(max_length=128)
+    processor = models.IntegerField()
+    theme = models.IntegerField()
+    aweber_list = models.CharField(max_length=128)
 
 class URL(models.Model):
     website = models.ForeignKey(Website)
@@ -13,5 +19,6 @@ class URL(models.Model):
     subdomain = models.CharField(max_length=255)
     
 class Level(models.Model):
+	website = models.ForeignKey(Website)
     name = models.CharField(max_length=128)
     number = models.IntegerField()
